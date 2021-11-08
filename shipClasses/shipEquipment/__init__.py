6fb1bb7848f63016e9e3fad151011d128b2ac5a1
ShadowAlzazel <92663497+ShadowAlzazel@users.noreply.github.com>
2021-11-08 14:49:41 -0800

    Add files via upload

--- shipClasses/shipEquipment/__init__.py
+++ shipClasses/shipEquipment/__init__.py
@@ -1,4 +1,5 @@
-print("shipEquipment Module")
+#print("shipEquipment Module")
 #from . shipWeapons import *
 from shipClasses.shipEquipment.shipWeapons import *
 from shipClasses.shipEquipment.shipDefenses import *
+from shipClasses.shipEquipment.shipRadar import *
